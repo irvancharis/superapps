3c200bbdc6f840dcadc9d647cf5d41eeb48d531a
SeptianDwiC <tiancahya755@gmail.com>
2025-01-30 15:37:44 +0700

    Maintenance SweetAlert

--- application/modules/ticket/views/ticket.php
+++ application/modules/ticket/views/ticket.php
@@ -81,8 +81,8 @@
                                                                 <div class="dropdown-menu">
                                                                     <a href="<?php echo base_url() . 'ticket/ticket_view/' . $d->IDTICKET ?>" class="dropdown-item has-icon view-btn"><i class="fas fa-eye"></i> View</a>
                                                                     <a href="<?php echo base_url() . 'ticket/edit_view/' . $d->IDTICKET ?>" class="dropdown-item has-icon edit-btn"><i class="far fa-edit"></i> Edit</a>
-                                                                    <a href="#" class="dropdown-item has-icon" id="update-approval" data-id="<?php echo $d->IDTICKET; ?>" data-approval="<?php echo $d->APPROVAL_TICKET; ?>"><i class="fas fa-hourglass-half"></i> Update Approval</a>
-                                                                    <a href="#" class="dropdown-item has-icon" id="update-status" data-id="<?php echo $d->IDTICKET; ?>" data-status="<?php echo $d->STATUS_TICKET; ?>"><i class="fas fa-hourglass-half"></i> Update Status</a>
+                                                                    <a href="#" class="dropdown-item has-icon update-approval" data-id="<?php echo $d->IDTICKET; ?>" data-approval="<?php echo $d->APPROVAL_TICKET; ?>"><i class="fas fa-hourglass-half"></i> Update Approval</a>
+                                                                    <a href="#" class="dropdown-item has-icon update-status" data-id="<?php echo $d->IDTICKET; ?>" data-status="<?php echo $d->STATUS_TICKET; ?>"><i class="fas fa-hourglass-half"></i> Update Status</a>
                                                                     <div class="dropdown-divider"></div>
                                                                     <a href="#" class="dropdown-item has-icon text-danger hapus-btn" data-id="<?php echo $d->IDTICKET; ?>" data-toggle="modal" data-target="#hapusModal"><i class="far fa-trash-alt"></i>
                                                                         Delete</a>
@@ -285,7 +285,7 @@
                     }
 
                     // Update Status Ticket
-                    $("#update-status").click(function() {
+                    $(".update-status").click(function() {
                         let id_ticket = $(this).data("id");
 
                         // Ambil status tiket dari atribut data
@@ -393,7 +393,7 @@
 
 
                     // Update Approval Ticket
-                    $("#update-approval").click(function() {
+                    $(".update-approval").click(function() {
                         let id_ticket = $(this).data("id");
 
                         // Ambil status tiket dari atribut data
