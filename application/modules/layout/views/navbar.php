bd85920cb4d6f0b785816506a69692118909f692
charis <irvancharis@gmail.com>
2025-02-11 07:48:23 +0700

    update sidebar

--- application/modules/layout/views/navbar.php
+++ application/modules/layout/views/navbar.php
@@ -176,7 +176,7 @@
                                 Settings
                             </a>
                             <div class="dropdown-divider"></div>
-                            <a href="auth-login.html" class="dropdown-item has-icon text-danger"> <i class="fas fa-sign-out-alt"></i>
+                            <a href="<?php echo base_url('logout'); ?>" class="dropdown-item has-icon text-danger"> <i class="fas fa-sign-out-alt"></i>
                                 Logout
                             </a>
                         </div>
