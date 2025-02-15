bc8aa4230bc1d28084c4f3143011d3afec9d4d43
SeptianDwiC <tiancahya755@gmail.com>
2025-02-15 07:50:56 +0700

    Update Produk Item

--- application/modules/produk_item/views/produk_item_tambah.php
+++ application/modules/produk_item/views/produk_item_tambah.php
@@ -14,14 +14,14 @@
                                                 <label>KODE PRODUK</label>
                                                 <input required type="text" name="KODE_ITEM" id="KODE_ITEM" class="form-control">
                                                 <div class="invalid-feedback">
-                                                Masukkan kode produk?
+                                                    Masukkan kode produk?
                                                 </div>
                                             </div>
                                             <div class="form-group col-12 col-md-6 col-lg-6">
                                                 <label>NAMA PRODUK</label>
                                                 <input required type="text" class="form-control" id="NAMA_ITEM" name="NAMA_ITEM">
                                                 <div class="invalid-feedback">
-                                                    Masukkan nama produk  !
+                                                    Masukkan nama produk !
                                                 </div>
                                             </div>
                                             <div class="form-group col-12 col-md-6 col-lg-6">
@@ -41,10 +41,10 @@
                                                 <label>SATUAN</label>
                                                 <input required type="text" class="form-control" id="SATUAN" name="SATUAN">
                                                 <div class="invalid-feedback">
-                                                    Masukkan nama produk  !
+                                                    Masukkan nama produk !
                                                 </div>
                                             </div>
-                                            
+
                                             <div class="form-group col-12 col-md-6 col-lg-6">
                                                 <label>KETERANGAN PRODUK</label>
                                                 <textarea required name="KETERANGAN_ITEM" placeholder="Masukkan keterangan produk" class="form-control" id="description_ticket"></textarea>
@@ -57,13 +57,14 @@
                                                 <label>FOTO</label>
                                                 <input required type="file" class="form-control" id="FOTO_ITEM" name="FOTO_ITEM" accept="image/gif, image/jpeg, image/png">
                                                 <div class="invalid-feedback">
-                                                    Masukkan FOTO  !
+                                                    Masukkan FOTO !
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="card-footer text-right">
-                                        <button class="btn btn-primary">Submit</button>
+                                        <button class="btn btn-primary"><i class="fa fa-save"></i> SIMPAN</button>
+                                        <a href="<?php echo base_url(); ?>produk_item" class="btn btn-secondary"><i class="fa fa-times"></i> BATAL</a>
                                     </div>
                                 </form>
                             </div>
@@ -71,18 +72,18 @@
                     </div>
                 </section>
             </div>
-            
+
 
             <?php $this->load->view('layout/footer'); ?>
 
             <script>
                 $(document).ready(function() {
-                    
+
                     // Input Area
                     $('#FORM_PRODUK_ITEM_TAMBAH').on('submit', function(e) {
                         e.preventDefault();
 
-                       // Ambil data dari form
+                        // Ambil data dari form
                         let formData = new FormData(this);
 
                         // Kirim data ke server melalui AJAX
@@ -107,7 +108,7 @@
                             }
                         });
                     });
-                    
+
                 });
             </script>
             </body>
